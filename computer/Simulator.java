7ee447680d4c6adc469e289789a8e233f364ab28
Alex Amado <alex-a1973@Live.com>
2020-05-17 14:17:50 -0700

    Simulator class: Contains ADDI instruction, adds it to instruction memeory, and executes. Implementation of execute coming soon

--- computer/Simulator.java
+++ computer/Simulator.java
@@ -10,7 +10,8 @@ public class Simulator {
 
 		// Add instructions
 		BitString addiInstr = new BitString();
-		addiInstr.setBits("1001100101111111".toCharArray());
+		// R[2] -> R[1] + 1
+		addiInstr.setBits("00100000001000100000000000000001".toCharArray());
 		comp.loadInstr(0, addiInstr);
 
 		comp.execute();
